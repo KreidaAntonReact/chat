051caeda27e959d3ad4ef4ce3e20fa77bb415eb2
Anton Kreida <kreida.anton@yandex.ru>
2025-05-07 19:32:12 +0500

    main | feature: add dev-server config for webpack

---
+++ packages/webpack/src/dev-server.webpack.config.ts
@@ -0,0 +1,16 @@
+import type { TOptionsBuild } from './lib';
+import type { Configuration } from 'webpack-dev-server';
+
+
+export const devServerWebpackConfig = ({ port, mode }: TOptionsBuild): Configuration | undefined => {
+  const isDev = mode === 'development';
+
+  return isDev
+    ? {
+      port: port,
+      open: true,
+      historyApiFallback: true,
+      hot: true
+    }
+    : undefined;
+};
