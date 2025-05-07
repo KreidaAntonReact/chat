261c110a748975201e8c3f901764277036d39e19
Anton Kreida <kreida.anton@yandex.ru>
2025-05-07 10:43:23 +0500

    main | feature: update eslint config

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -7,6 +7,8 @@ module.exports = {
   },
   extends: [
     "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
   ],
   settings: {
     "import/resolver": {
@@ -16,9 +18,12 @@ module.exports = {
     }
   },
   plugins: [
-    "@typescript-eslint"
+    "@typescript-eslint",
+    "import",
+    "unused-imports"
   ],
   rules: {
+    // Базовые правила для typescript
     "@typescript-eslint/interface-name-prefix": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
@@ -28,6 +33,105 @@ module.exports = {
       {
         "argsIgnorePattern": "^_",
         "varsIgnorePattern": "^_"
+      }
+    ],
+
+    // Стилизация кода
+    "arrow-body-style": ["error", "as-needed"],
+    "arrow-parens": ["error", "always"],
+    "block-spacing": "error",
+    "comma-dangle": ["error", "only-multiline"],
+    "eol-last": ["error", "always"],
+    "function-paren-newline": ["error", "consistent"],
+    "indent": ["error", 2, { "SwitchCase": 1 }],
+    "max-len": ["warn", { "code": 160, "ignoreUrls": true }],
+    "multiline-ternary": ["error", "always-multiline"],
+    "no-multiple-empty-lines": ["error", { "max": 2, "maxBOF": 0 }],
+    "quotes": ["error", "single", { "avoidEscape": true }],
+    "semi": ["error", "always"],
+
+    // Стиль объектов
+    "object-curly-newline": [
+      "error",
+      {
+        "ObjectExpression": {
+          "minProperties": 4,
+          "multiline": true,
+          "consistent": true
+        },
+        "ObjectPattern": {
+          "minProperties": 4,
+          "multiline": true,
+          "consistent": true
+        },
+        "ImportDeclaration": {
+          "minProperties": 4,
+          "multiline": true,
+          "consistent": true
+        },
+        "ExportDeclaration": {
+          "minProperties": 4,
+          "multiline": true,
+          "consistent": true
+        }
+      }
+    ],
+    "object-curly-spacing": ["error", "always"],
+    "object-property-newline": [
+      "error",
+      {
+        "allowAllPropertiesOnSameLine": true
+      }
+    ],
+
+    // Импорты
+    "import/no-unresolved": "error",
+    "import/no-cycle": "error",
+    "import/order": [
+      "error",
+      {
+        "groups": [
+          "builtin",
+          "external",
+          "internal",
+          ["sibling", "parent"],
+          "index",
+          "object",
+          "type"
+        ],
+        "newlines-between": "always",
+        "alphabetize": {
+          "order": "asc",
+          "caseInsensitive": true
+        }
+      }
+    ],
+    "import/no-useless-path-segments": [
+      "error",
+      {
+        "noUselessIndex": true
+      }
+    ],
+    "import/newline-after-import": ["error", { "count": 1 }],
+    "import/exports-last": "error",
+
+    // Неиспользуемые импорты
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        "vars": "all",
+        "varsIgnorePattern": "^_",
+        "args": "after-used",
+        "argsIgnorePattern": "^_"
+      }
+    ],
+
+    // Отладка
+    "no-console": [
+      "warn",
+      {
+        "allow": ["warn", "error"]
       }
     ]
   },
--- packages/webpack/src/lib/types/config-webpack.type.ts
+++ packages/webpack/src/lib/types/config-webpack.type.ts
@@ -1,4 +1,4 @@
-import type {Configuration } from 'webpack';
-import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
+import type { Configuration } from 'webpack';
+import type { Configuration as DevServerConfiguration } from 'webpack-dev-server';
 
 export type TConfigWebpack = Configuration & DevServerConfiguration;
