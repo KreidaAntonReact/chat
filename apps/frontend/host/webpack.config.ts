f34fd3c167c06ee40a6227d95e24fc57277b2ecf
Anton Kreida <kreida.anton@yandex.ru>
2025-05-09 20:41:58 +0500

    main | feature: init microfrontend host

--- apps/frontend/host/webpack.config.ts
+++ apps/frontend/host/webpack.config.ts
@@ -1,11 +1,15 @@
-import "dotenv/config";
+import 'dotenv/config';
 import path from 'path';
 
 import { type TConfigWebpack, buildWebpackConfig } from '@packages/webpack';
+import { container } from 'webpack';
+
+import packageJson from './package.json';
+
 
 export default (): TConfigWebpack => {
   const config = buildWebpackConfig({
-    mode: process.env.NODE_ENV as "development" | "production" || "development",
+    mode: process.env.NODE_ENV as 'development' | 'production' || 'development',
     port: Number(process.env.PORT) || 3000,
     paths: {
       output: path.resolve(__dirname, 'dist'),
@@ -19,6 +23,24 @@ export default (): TConfigWebpack => {
       '@': path.resolve(__dirname, 'src'),
     },
   });
+
+
+  config.plugins?.push(new container.ModuleFederationPlugin({
+    name: 'host',
+    filename: 'remoteEntry.js',
+    shared: {
+      ...packageJson.dependencies,
+      react: {
+        eager: true,
+      },
+      'react-router': {
+        eager: true,
+      },
+      'react-dom': {
+        eager: true,
+      }
+    }
+  }));
 
   return config;
 };
