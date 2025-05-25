6f953880edc399b95a9b9856e989d40187d11674
Anton Kreida <kreida.anton@yandex.ru>
2025-05-25 19:25:44 +0500

    main | fix: fix hook

--- apps/frontend/chat-list/src/features/change-filter-chats/model/use-change-filters/use-change-filters.ts
+++ apps/frontend/chat-list/src/features/change-filter-chats/model/use-change-filters/use-change-filters.ts
@@ -6,12 +6,12 @@ import type { ListOptionsFilters } from '@/features/change-filter-chats/lib';
 type TOptionsValue = typeof ListOptionsFilters[number]['value'];
 
 
-interface IUseChangeFiltersArgs {
-    defaultValue: TOptionsValue
+interface IUseChangeFiltersArgs<T extends string> {
+    defaultValue: T
 }
 
-export const useChangeFilters = ({ defaultValue }: IUseChangeFiltersArgs) => {
-  const [currentOption, setCurrentOption] = useState<TOptionsValue>(defaultValue);
+export const useChangeFilters = ({ defaultValue }: IUseChangeFiltersArgs<TOptionsValue>) => {
+  const [currentOption, setCurrentOption] = useState(defaultValue);
 
   const handleOnChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value as TOptionsValue;
